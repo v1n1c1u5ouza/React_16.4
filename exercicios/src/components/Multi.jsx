772824a2226d9f418d78c203f0641a80a051f4f8
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-11-29 12:49:26 -0300

    Multiples components exercise

---
+++ exercicios/src/components/Multi.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const BoaTarde = (props) => <h1>Boa tarde {props.name}!</h1>;
+
+const BoaNoite = (props) => <h1>Boa noite {props.name}!</h1>;
+
+export { BoaTarde, BoaNoite };
+export default { BoaTarde, BoaNoite };
--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom";
 
 import First from "./components/First";
 import GoodMorning from "./components/GoodMorning";
+import Multi, { BoaTarde } from "./components/Multi";
 
-ReactDOM.render(<GoodMorning name= "Matheus" />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <Multi.BoaNoite name="Ana" />
+    <BoaTarde name="Bia" />
+  </div>,
+  document.getElementById("root")
+);
