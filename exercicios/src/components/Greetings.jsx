bb9ce971933069465d35f6956c3ddcfb305f3162
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-30 08:44:27 -0300

    Class components exercise

---
+++ exercicios/src/components/Greetings.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export default class Greetings extends Component {
+  state = {
+    type: this.props.type,
+    name: this.props.name,
+  };
+
+  constructor(props) {
+    super(props);
+    this.setType = this.setType.bind(this);
+  }
+
+  setType(e) {
+    this.setState({ type: e.target.value });
+  }
+
+  setName(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  render() {
+    const { type, name } = this.state;
+    return (
+      <div>
+        <h1>
+          {type} {name}
+        </h1>
+        <hr />
+        <input
+          type="text"
+          placeholder="Tipo....."
+          value={type}
+          onChange={this.setType}
+        />
+        <input
+          type="text"
+          placeholder="Nome....."
+          value={name}
+          onChange={(e) => this.setName(e)}
+        />
+      </div>
+    );
+  }
+}
--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 
 import First from "./components/First";
 import GoodMorning from "./components/GoodMorning";
+import Greetings from "./components/Greetings";
 import Multi, { BoaTarde } from "./components/Multi";
 
 ReactDOM.render(
   <div>
-    <Multi.BoaNoite name="Ana" />
-    <BoaTarde name="Bia" />
+    <Greetings />
   </div>,
   document.getElementById("root")
 );
