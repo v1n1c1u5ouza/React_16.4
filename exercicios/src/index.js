088d48a4d97d1fe46442acc762a0149c81947f71
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-30 09:43:32 -0300

    Parent and child components exercise

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Child from "./components/Child";
 import First from "./components/First";
 import GoodMorning from "./components/GoodMorning";
 import Greetings from "./components/Greetings";
 import Multi, { BoaTarde } from "./components/Multi";
+import Parent from "./components/Parent";
 
 ReactDOM.render(
   <div>
-    <Greetings />
+    <Parent name="Amanda" sobrenome="Fernades">
+        <Child name="Paulo"/>
+        <Child name="Maria"/>
+        <Child name="Carlos"/>
+    </Parent>
   </div>,
   document.getElementById("root")
 );
